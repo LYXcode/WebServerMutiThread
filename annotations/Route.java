cbdb0a7b23260331b0d9a942f77d7549e9b1a43a
LYXcode <1367527584@qq.com>
2021-05-14 23:50:04 +0800

    modify annotation

--- annotations/Route.java
+++ annotations/Route.java
@@ -1,15 +1,13 @@
+package annotations;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
-@Target(ElementType.FIELD)
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Inherited
 public @interface Route {
     String route() default "";
 }
---
+++ workers/MyWorker.java
@@ -0,0 +1,7 @@
+package workers;
+
+import annotations.*;
+@Worker
+public class MyWorker {
+    
+}
