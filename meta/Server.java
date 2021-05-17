f184ce48bf87218cc957fd18625c1c5ad5b138fa
LYXcode <1367527584@qq.com>
2021-05-17 22:42:29 +0800

    add threadpool

--- meta/Server.java
+++ meta/Server.java
@@ -2,11 +2,21 @@ package meta;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Time;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Server {
     private int port;
     private ServerSocket serverSocket;
     private WorkerManager workerManager;
+
+    private static final int CORE_POOL_SIZE = 5;
+    private static final int MAX_POOL_SIZE = 10;
+    private static final int QUEUE_CAPACITY = 100;
+    private static final Long KEEP_ALIVE_TIME = 1L;
+    
 
     public Server(int port) throws ClassNotFoundException {
         this.port = port;
@@ -21,11 +31,17 @@ public class Server {
     }
 
     public void startServer() throws IOException{
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, new ArrayBlockingQueue<>(QUEUE_CAPACITY), new ThreadPoolExecutor.CallerRunsPolicy());
+
         while(true){
             Socket socket = this.serverSocket.accept();
             SocketHandler socketHandler = new SocketHandler(socket, workerManager);
-            Thread handleThread = new Thread(socketHandler);
-            handleThread.start();
+            // Thread handleThread = new Thread(socketHandler);
+            // handleThread.start();
+
+            executor.execute(socketHandler);
         }
+
     }
 }
